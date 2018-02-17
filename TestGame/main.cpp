adab3be0a5a6ea0e26d1cd06149360af7a49af51
Máté Czagány <czmate10@gmail.com>
2018-02-17 14:32:52 +0100

    Added simple camera movement to TestGame

--- TestGame/main.cpp
+++ TestGame/main.cpp
@@ -1,27 +1,94 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 #include "Engine.h"
 #include "Render/Mesh/Sphere.h"
 #include "Render/Mesh/Cube.h"
 #include "Components/MeshComponent.h"
 #include "Components/CameraComponent.h"
+#include "Events/EventTypes/EventTick.h"
 #include "Events/EventTypes/EventKeyPressed.h"
+#include "Events/EventTypes/EventCursorMoved.h"
 #include "Render/Mesh/Quad.h"
 #include "Components/LightComponent.h"
 
+// Constants for quick calculations
+const float PI_HALF = glm::pi<float>()/2;
 
+// Constants to control movement speed
+const float MOVE_SPEED = 0.05f;
+const float MOUSE_SENS = 0.003f;
+
+// Our camera object
+Ptah::Entity* entity_camera;
+
+// Variables for looking around
+float mouse_last_x = 400;
+float mouse_last_y = 300;
+
+// Camera rotation
+void on_cursor_moved(Ptah::Event* ev_)
+{
+	auto ev = static_cast<Ptah::EventCursorMoved*>(ev_);
+	float x = (float)ev->mouse_x;
+	float y = (float)ev->mouse_y;
+
+	float offset_x = (x - mouse_last_x) * MOUSE_SENS;
+	float offset_y = (y - mouse_last_y) * MOUSE_SENS;
+	mouse_last_x = x;
+	mouse_last_y = y;
+
+	auto rotation = entity_camera->GetTransform()->GetRotation();
+	rotation.x += offset_x;
+	rotation.y -= offset_y;
+
+	if(rotation.y > PI_HALF)
+		rotation.y = PI_HALF;
+	else if(rotation.y < -PI_HALF)
+		rotation.y = -PI_HALF;
+
+
+	entity_camera->GetTransform()->SetRotation(rotation);
+}
+
+// Camera movement
+void on_tick(Ptah::Event* ev_)
+{
+	auto ev = static_cast<Ptah::EventTick*>(ev_);
+	auto timeF = (float)ev->time;
+	auto transform = entity_camera->GetTransform();
+	
+	if(Ptah::Engine::Instance().GetWindow()->GetKey(GLFW_KEY_W) == GLFW_PRESS)
+		transform->SetPos(transform->GetPos() + (transform->GetFront() * MOVE_SPEED * timeF));
+	
+	if(Ptah::Engine::Instance().GetWindow()->GetKey(GLFW_KEY_S) == GLFW_PRESS)
+		transform->SetPos(transform->GetPos() - (transform->GetFront() * MOVE_SPEED * timeF));
+
+	if(Ptah::Engine::Instance().GetWindow()->GetKey(GLFW_KEY_D) == GLFW_PRESS)
+		transform->SetPos(transform->GetPos() + (transform->GetRight() * MOVE_SPEED * timeF));
+
+	if(Ptah::Engine::Instance().GetWindow()->GetKey(GLFW_KEY_A) == GLFW_PRESS)
+		transform->SetPos(transform->GetPos() - (transform->GetRight() * MOVE_SPEED * timeF));
+}
+
+// Close window when user presses escape
 void on_key_pressed(Ptah::Event* ev_)
 {
 	auto ev = static_cast<Ptah::EventKeyPressed*>(ev_);
-	std::cout << ev->key_ << std::endl;
+
+	if(ev->key == GLFW_KEY_ESCAPE)
+		Ptah::Engine::Instance().Shutdown();
 }
 
 void init()
 {
 	Ptah::Engine::Instance().Init();
 	Ptah::Engine::Instance().SetWindowTitle("Testing Ptah OpenGL engine");
-
+	
+	Ptah::Engine::Instance().GetEventDispatcher()->AddEventListener("tick", &on_tick);
 	Ptah::Engine::Instance().GetEventDispatcher()->AddEventListener("key_pressed", &on_key_pressed);
+	Ptah::Engine::Instance().GetEventDispatcher()->AddEventListener("cursor_moved", &on_cursor_moved);
 
 	Ptah::World world;
 	Ptah::Engine::Instance().SetWorld(&world);
@@ -55,11 +122,11 @@ void init()
 	}
 
 	// Camera
-	auto entity_camera = world.GetEntityManager().Add("player_camera");
-	auto camera_comp = new Ptah::CameraComponent();
+	entity_camera = world.GetEntityManager().Add("player_camera");
+	Ptah::CameraComponent* camera_comp = new Ptah::CameraComponent();
 	camera_comp->SetActive();
 	entity_camera->AddComponent<Ptah::CameraComponent>(camera_comp);
-	entity_camera->GetTransform()->SetPos(glm::vec3(5.0f, 5.0f, 5.0f));
+	entity_camera->GetTransform()->SetPos(glm::vec3(0.0f, 0.0f, 3.0f));
 
 	Ptah::Engine::Instance().StartLoop();
 
